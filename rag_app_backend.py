f4cb86e693266764c4f25ab93aa51b6af2da1974
klan86at <klang86at@gmail.com>
2025-11-18 16:36:39 +0300

    package libraries added

--- rag_app_backend.py
+++ rag_app_backend.py
@@ -1,1 +1,12 @@
-from dotenv
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import json
+import requests
+from uuid import uuid4
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
